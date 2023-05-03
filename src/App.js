f750d5dad01faffbe1460d01040b9117f3092070
Michael Liu <micliu@ucdavis.edu>
2023-05-02 21:32:28 -0700

    Third Commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+//import logo from '.
+import '/.App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.jss</code> and save to reload
+        </p>
+        <a
+          className="App-link"
+          her="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Reactt
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
